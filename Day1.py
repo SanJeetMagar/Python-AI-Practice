25d0fbd0a6d48e376fe9a46503360c6d20a2df12
SanJeetMagar <voidisstartingtocode@gmail.com>
2026-04-15 22:34:38 +0545

    initial commit without venv

---
+++ Day1.py
@@ -0,0 +1,17 @@
+#Question 1
+# my_text = "Hello, My name is Sanjeet Thapa"
+# for i in my_text.split(" "):
+#     print(i)
+
+#Question 2
+# x,y = input().split(" ")
+# even_num = list(range(100,200,2))
+# print(even_num)
+
+#Question 3
+string = input()
+X = input()
+Y = input()
+print(string.replace(X, Y))
+
+print("hello world")
